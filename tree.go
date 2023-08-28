93747b29180b7cfe958c6d95323e3043c913f7af
Stas Piter <stas@forji.org>
2023-08-28 00:58:34 +0000

    Add watch

--- tree.go
+++ tree.go
@@ -1,15 +1,28 @@
 package forjitree
 
+import (
+	"sync"
+	"time"
+)
+
 type Tree struct {
 	objectTypes map[string]*ObjectType
 	rootNode    *node
 	created     bool
+
+	watchers               map[string]*watcher
+	watchersMutex          sync.Mutex
+	watchersCleanTimestamp time.Time
+	watchersCleanInterval  float64
 }
 
 func New() *Tree {
 	t := &Tree{
-		objectTypes: make(map[string]*ObjectType),
-		created:     false,
+		objectTypes:            make(map[string]*ObjectType),
+		created:                false,
+		watchers:               make(map[string]*watcher),
+		watchersCleanTimestamp: time.Now(),
+		watchersCleanInterval:  60,
 	}
 	t.rootNode = newNode(t, nil, "")
 	return t
@@ -25,6 +38,13 @@ func (t *Tree) Created() {
 
 func (t *Tree) DestroyObjects() {
 	t.rootNode.destroyObject(true)
+}
+
+func (t *Tree) Clear() {
+	t.watchers = make(map[string]*watcher)
+	t.DestroyObjects()
+	t.rootNode.callCreatedTree()
+	t.created = false
 }
 
 func (t *Tree) GetValue() any {
@@ -50,6 +70,41 @@ func (t *Tree) Set(data any) {
 		for i := 0; i < len(createdObjects); i++ {
 			createdObjects[i].obj.CreatedTree()
 		}
+	}
+
+	// Merge with watchers changes
+	t.watchersMutex.Lock()
+	for _, w := range t.watchers {
+		w.collectChanges(data)
+	}
+	t.watchersMutex.Unlock()
+}
+
+func (t *Tree) Watch(watcherId string) any {
+	// Clean watchers routine - delete those ones which haven't been accessed for longer than watchersCleanInterval
+	if time.Since(t.watchersCleanTimestamp).Seconds() > t.watchersCleanInterval/2 {
+		t.watchersCleanTimestamp = time.Now()
+		t.watchersMutex.Lock()
+		for wid, w := range t.watchers {
+			if time.Since(w.extractTimestamp).Seconds() > t.watchersCleanInterval {
+				delete(t.watchers, wid)
+			}
+		}
+		t.watchersMutex.Unlock()
+	}
+
+	t.watchersMutex.Lock()
+	w, watcherExists := t.watchers[watcherId]
+
+	if watcherExists {
+		// Extract collected changes if watcher exists
+		t.watchersMutex.Unlock()
+		return w.extractChanges()
+	} else {
+		// Otherwise return full value and create a new watcher
+		t.watchers[watcherId] = newWatcher(watcherId)
+		t.watchersMutex.Unlock()
+		return t.GetValue()
 	}
 }
 
---
+++ watcher.go
@@ -0,0 +1,38 @@
+package forjitree
+
+import (
+	"sync"
+	"time"
+)
+
+type watcher struct {
+	watcherId        string
+	tree             *Tree
+	extractTimestamp time.Time
+	mu               sync.Mutex
+}
+
+func newWatcher(watcherId string) *watcher {
+	w := &watcher{
+		watcherId:        watcherId,
+		tree:             New(),
+		extractTimestamp: time.Now(),
+	}
+	w.tree.created = true
+	return w
+}
+
+func (w *watcher) collectChanges(changes any) {
+	w.mu.Lock()
+	w.tree.Set(changes)
+	w.mu.Unlock()
+}
+
+func (w *watcher) extractChanges() any {
+	w.mu.Lock()
+	w.extractTimestamp = time.Now()
+	result := w.tree.GetValue()
+	w.tree.rootNode = newNode(w.tree, nil, "")
+	w.mu.Unlock()
+	return result
+}
