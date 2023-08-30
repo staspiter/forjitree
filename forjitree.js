b12060d0782ed372623519f145bd20b7dde5a52a
Stas Piter <stas@forji.org>
2023-08-30 08:06:56 +0000

    Forjitree.js initial commit and test

--- forjitree.js
+++ forjitree.js
@@ -5,7 +5,6 @@ class Tree {
         this.rootNode = new Node(this, null, "")
         this.created = false
         this.modified = false
-
     }
 
     Created() {
@@ -37,12 +36,12 @@ class Tree {
 
         // Call CreatedChildren
         for (let i = createdObjects.length - 1; i >= 0; i--)
-            createdObjects[i].callObj('CreatedChildren')
+            createdObjects[i].callObjFunc('CreatedChildren')
 
         // Call CreatedTree if the tree has already been created
         if (this.created) 
             for (const o of createdObjects)
-                o.callObj('CreatedTree')
+                o.callObjFunc('CreatedTree')
 
         if (modifiedNodes.length > 0)
             this.modified = true
@@ -73,34 +72,359 @@ class Tree {
 
 }
 
+const NodeType = {
+    Map: 0,
+    Slice: 1,
+    Value: 2,
+}
+
+const ObjectKeyword = "object"
+
 class Node {
 
     constructor(tree, parent, parentKey) {
-        
+        this.tree = tree
+        this.parent = parent
+        this.parentKey = parentKey
+        this.nodeType = NodeType.Value
+        this.m = {}
+        this.sl = []
+        this.value = null
+        this.obj = null
+        this.objType = null
     }
 
-    callCreatedTree() {
+    setNodeType(newNodeType) {
+        if (newNodeType == this.nodeType)
+            return false
 
-    }
+        this.destroyObject(true)
 
-    destroyObject(callNested) {
+        this.m = {}
+        this.sl = []
+        this.value = null
 
-    }
+        this.nodeType = newNodeType
 
-    callObj(funcName) {
-        
+        return true
     }
 
     getValue() {
-
+        if (this.nodeType == NodeType.Map) {
+            let m = {}
+            for (const [k, v] of Object.entries(this.m))
+                m[k] = v.getValue()
+            return m
+        } else if (this.nodeType == NodeType.Slice) {
+            let sl = []
+            for (const v of this.sl)
+                sl.push(v.getValue())
+            return sl
+        } else {
+            return this.value
+        } 
     }
 
+    patch(d) {
+        let modified = false
+        let modifiedSubnodes = []
+
+        if (typeof d === 'object' && d !== null) {
+            modified = this.setNodeType(NodeType.Map)
+            for (const [k, v] of Object.entries(d)) {
+                let n = this.m[k]
+                if (!n) {
+                    n = new Node(this.tree, this, k)
+                    this.m[k] = n
+                    modified = true
+                }
+                modifiedSubnodes = modifiedSubnodes.concat(n.patch(v))
+            }
+
+        } else if (d.constructor === Array) {
+            modified = this.setNodeType(NodeType.Slice)
+            for (let i = 0; i < d.length; i++) {
+                let v = d[i]
+                if (this.sl.length <= i) {
+                    let n = new Node(this.tree, this, i)
+                    this.sl.push(n)
+                    modified = true
+                }
+                let n = this.sl[i]
+                modifiedSubnodes = modifiedSubnodes.concat(n.patch(v))
+            }
+            if (this.sl.length > d.length) {
+                for (let i = d.length; i < this.sl.length; i++)
+                    this.sl[i].destroyObject(true)
+                this.sl = this.sl.slice(0, d.length)
+                modified = true
+            }
+
+        } else {
+            modified = this.setNodeType(NodeType.Value)
+            if (this.value != d)
+                modified = true
+            this.value = d
+        }
+
+        if (modified)
+            modifiedSubnodes.push(this)
+
+        return modifiedSubnodes
+    }
+
+    synchronize() {
+        let createdObj = false
+
+        let newType = null
+        if (this.nodeType == NodeType.Map) {
+            let typeNode = this.m[ObjectKeyword]
+            if (typeNode && typeNode.nodeType == NodeType.Value && typeNode.value != null)
+                newType = this.tree.GetType(typeNode.value)
+        }
+
+        if (newType != this.objType) {
+            if (this.objType)
+                this.destroyObject(false)
+            
+            if (newType) {
+                this.objType = newType
+                this.obj = new newType.createObject(this)
+                this.obj.node = this
+
+                // Set all fields immediately before calling Created
+                for (const [k, v] of Object.entries(this.m)) {
+                    if (k == ObjectKeyword)
+                        continue
+                    this.objType.setField(this, k, v.getValue())
+                }
+
+                this.callObjFunc('Created')
+                createdObj = true
+            }
+        }
+
+        if (this.parent != null && this.parent.nodeType == NodeType.Map && this.parent.objType != null && this.parentKey != ObjectKeyword)
+            this.parent.objType.setField(this.parent, this.parentKey, this.getValue())
+        
+        return createdObj
+    }
+
+    destroyObject(callNested) {
+        if (callNested) {
+            for (const [k, v] of Object.entries(this.m))
+                v.destroyObject(true)
+            for (const v of this.sl)
+                v.destroyObject(true)
+        }
+
+        if (this.objType) {
+            this.callObjFunc('Destroyed')
+            this.objType = null
+            this.obj = null
+        }
+    }
+
+    callCreatedTree() {
+        if (this.objType) 
+            this.callObjFunc('CreatedTree')
+        for (const [k, v] of Object.entries(this.m))
+            v.callCreatedTree()
+        for (const v of this.sl)
+            v.callCreatedTree()
+    }
+
+    callObjFunc(funcName) {
+        if (!this.obj || !this.obj[funcName] || typeof this.obj[funcName] !== "function")
+            return
+        this.obj[funcName]()
+    }
+
+    getParent() {
+        return this.parent
+    }
+
+    getChild(key) {
+        if (this.nodeType == NodeType.Map)
+            return this.m[key]
+        else if (this.nodeType == NodeType.Slice)
+            return this.sl[key]
+        else
+            return null
+    }
+
+    getChildren(recursive) {
+        let result = []
+        if (this.nodeType == NodeType.Map) {
+            for (const [k, v] of Object.entries(this.m)) {
+                result.push(v)
+                if (recursive)
+                    result = result.concat(v.getChildren(true))
+            }
+        } else if (this.nodeType == NodeType.Slice) {
+            for (const v of this.sl) {
+                result.push(v)
+                if (recursive)
+                    result = result.concat(v.getChildren(true))
+            }
+        }
+        return result
+    }
+
+    getParents() {
+        let result = []
+        let p = this.parent
+        while (p) {
+            result.push(p)
+            p = p.parent
+        }
+        return result
+    }
+
+    internalGet(nodes, t, postProcess) {
+        let result = []
+
+        let appendPostprocess = (n) => {
+            if (n == null)
+                return
+            let appendArr = [n]
+            if (n.value && (typeof n.value === 'string' || n.value instanceof String) && n.value.startsWith('@') && n.parent != null) {
+                let subResult = n.parent.Get(n.value.substring(1), true)
+                appendArr = []
+                for (const sn of subResult)
+                    appendArr.push(sn)
+            }
+            for (const an of appendArr) {
+                let exists = false
+                for (const r of result)
+                    if (r == an) {
+                        exists = true
+                        break
+                    }
+                if (!exists)
+                    result.push(an)
+            }
+        }
+
+        for (const n of nodes) {
+
+            if (t.kind == PathTokenKind.This)
+                appendPostprocess(n)
+            
+            else if (t.kind == PathTokenKind.Parent)
+                appendPostprocess(n.parent)
+
+            else if (t.kind == PathTokenKind.AllParents) {
+                let parents = n.getParents()
+                for (const p of parents)
+                    appendPostprocess(p)
+            }
+
+            else if (t.kind == PathTokenKind.Root)
+                appendPostprocess(n.tree.rootNode)
+
+            else if (t.kind == PathTokenKind.Sub)
+                appendPostprocess(n.getChild(t.key))
+
+            else if (t.kind == PathTokenKind.Params) {
+                let satisfied = true
+                for (const p of t.params) {
+                    if (p.key == "PARENT_KEY") {
+                        if (n.parentKey != p.value) {
+                            satisfied = false
+                            break
+                        }
+                    } else {
+                        let n1 = n.getChild(p.key)
+                        if (!n1 || (p.value != "" && n1.value != p.value)) {
+                            satisfied = false
+                            break
+                        }
+                    }
+                }
+                if (satisfied)
+                    appendPostprocess(n)
+            }
+
+            else if (t.kind == PathTokenKind.DirectChildren) {
+                let subs = n.getChildren(false)
+                for (const s of subs)
+                    appendPostprocess(s)
+            }
+
+            else if (t.kind == PathTokenKind.AllChildren) {
+                let subs = n.getChildren(true)
+                for (const s of subs)
+                    appendPostprocess(s)
+            }
+        }
+
+        return result
+    }
+
+    Get(path, postProcess) {
+        let tokenizedPath = tokenizePath(path)
+
+        if (tokenizedPath.length == 0)
+            return []
+
+        let tempResult = [this]
+        for (const t of tokenizedPath)
+            tempResult = this.internalGet(tempResult, t, postProcess)
+
+        return tempResult
+    }
+
+    Value() {
+        return this.getValue()
+    }
+
+    Parent() {
+        return this.getParent()
+    }
+
+    Root() {
+        return this.tree.rootNode
+    }
+
+    Name() {
+        return this.parentKey
+    }
+
+    Path() {
+        if (this.parent == null)
+            return ""
+        return this.parent.Path() + "/" + this.parentKey
+    }
+
+    Tree() {
+        return this.tree
+    }
+
+    Set(newValue) {
+        this.tree.Set(makePatchWithPath(this.Path().substring(1), newValue))
+    }
+
+    NodeType() {
+        return this.nodeType
+    }
 }
 
 class ObjectType {
 
     constructor(objectClass, name) {
+        this.objectClass = objectClass
+        this.name = name
+    }
 
+    createObject(node) {
+        return new this.objectClass(node)
+    }
+
+    setField(node, key, value) {
+        node.obj[key] = value
+        if (node.obj["Updated"] && typeof node.obj["Updated"] === "function")
+            node.obj["Updated"](key, value)
     }
 
 }
@@ -113,7 +437,7 @@ const PathTokenKind = {
     Params: 4,
     DirectChildren: 5,
     AllChildren: 6,
-    AllParents: 7
+    AllParents: 7,
 }
 
 function tokenizePath(path) {
@@ -196,6 +520,62 @@ function tokenizePath(path) {
     return tokens
 }
 
+function makePatchWithPath(path, object) {
+    if (path == "")
+        return object
+    let pathArr = path.split('/')
+    let m = {}
+    let m1 = m
+    for (let i = 0; i < pathArr.length; i++) {
+        if (i == pathArr.length - 1)
+            m1[pathArr[i]] = object
+        else {
+            m1[pathArr[i]] = {}
+            m1 = m1[pathArr[i]]
+        }
+    }
+    return m
+}
+
+function GetObjs(nodes) {
+    let result = []
+    for (const n of nodes)
+        if (n.obj)
+            result.push(n.obj)
+    return result
+}
+
+function GetObj(nodes) {
+    for (const n of nodes)
+        if (n.obj)
+            return n.obj
+    return null
+}
+
 class DatasourceClient {
+
+    constructor(node) {
+        this.node = node
+        this.websocket = false
+        this.url = ""
+        this.tree = new Tree()
+    }
+
+    Created() {
+        if (this.websocket) {
+            this.watcherId = crypto.randomUUID()
+            this.socket = new WebSocket(this.url + "/" + this.watcherId)
+            this.socket.onmessage = (event) => {
+                let data = JSON.parse(event.data)
+                this.tree.Set(data)
+                console.log(data)
+            }
+            
+        } else {
+            fetch(this.url)
+                .then(response => response.json())
+                .then(data => this.tree.Set(data))
+        }
+    }
 
 }
