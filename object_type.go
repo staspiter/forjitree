b75efbae30ad5c1eb0a8660fda4c00a559d819c3
Stas Piter <stas@forji.org>
2023-09-18 22:04:41 +0000

    Remove commented code

--- object_type.go
+++ object_type.go
@@ -95,17 +95,6 @@ func (t *ObjectType) setField(n *node, fieldName string, fieldValue any) {
 	}
 
 	n.obj.Updated(fieldName, fieldValue)
-	/*
-		m := n.objReflect.MethodByName("Updated")
-		if m.IsValid() && !m.IsZero() {
-			if fieldValue == nil {
-				// A workaround for nil values, because reflect.Value does not support nil values
-				m.Call([]reflect.Value{reflect.ValueOf(fieldName), NilReflectValue})
-			} else {
-				m.Call([]reflect.Value{reflect.ValueOf(fieldName), reflect.ValueOf(fieldValue)})
-			}
-		}
-	*/
 }
 
 func (t *ObjectType) callRedirect(n *node) []*node {
